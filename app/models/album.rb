NguyenDangVan/develop

init model & add relationship

app/models/album.rb
@@ -1,0 +1,5 @@
+class Album < ApplicationRecord
+  belongs_to :category
+  belongs_to :artist
+  has_many :song
+end

app/models/artist.rb
@@ -1,0 +1,3 @@
+class Artist < ApplicationRecord
+  has_many :album
+end

app/models/category.rb
@@ -1,0 +1,3 @@
+class Category < ApplicationRecord
+  has_many :album
+end

app/models/playlist.rb
@@ -1,0 +1,4 @@
+class Playlist < ApplicationRecord
+  belongs_to :user
+  has_many :playlist_song
+end

app/models/song.rb
@@ -1,0 +1,5 @@
+class Song < ApplicationRecord
+  belongs_to :album
+  has_many :playlist_song
+  has_many :favorite
+end

awesome-academy/R-Han-OE11-P1-Group

app/models/album.rb
@@ -1,0 +1,5 @@
+class Album < ApplicationRecord
+  belongs_to :category
+  belongs_to :artist
+  has_many :songs
+end

app/models/artist.rb
@@ -1,0 +1,3 @@
+class Artist < ApplicationRecord
+  has_many :albums
+end

app/models/category.rb
@@ -1,0 +1,3 @@
+class Category < ApplicationRecord
+  has_many :albums
+end

app/models/playlist.rb
@@ -1,0 +1,4 @@
+class Playlist < ApplicationRecord
+  belongs_to :user
+  has_many :playlist_songs
+end

app/models/song.rb
@@ -1,0 +1,5 @@
+class Song < ApplicationRecord
+  belongs_to :album
+  has_many :playlist_songs
+  has_many :favorites
+end